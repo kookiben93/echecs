2c60ef93445574b44aa6d738f40017136fe17a5e
Achille444 <achille.valesc@gmail.com>
2025-12-28 00:09:51 +0100

    Refactor player choice and game mode logic

--- 27decembre/Main.java
+++ 27decembre/Main.java
@@ -8,9 +8,24 @@ public class Main {
 
     methodes.plateau(plateau);
 
-    String pseudoBlanc="";
-    String pseudoNoir="";
+    String pseudoBlanc = "";
+    String pseudoNoir = "";
     int modeJeu;
+    int choix = 0;
+
+    System.out.println("BIENVENUE");
+    while (choix != 1 && choix != 2 && choix != 3) {
+      choix = methodes.debut(choix);
+    }
+    while (choix != 1) {
+      if (choix == 2) {
+        System.out.println("Indisponible pour le moment");
+        choix = methodes.debut(choix);
+      } else {
+        System.out.println("Pourquoi ???");
+        choix = methodes.debut(choix);
+      }
+    }
 
     System.out.print("Tapez 1 pour le mode débutant, tapez 2 pour le mode normal : ");
     modeJeu = Integer.parseInt(scanner.nextLine());
@@ -21,28 +36,28 @@ public class Main {
     System.out.print("Joueur 2 entrez votre pseudo : ");
     String pseudo2 = scanner.nextLine();
 
-    System.out.print(pseudo1 + " veux-tu jouer les bleus ou les jaunes ? ");
-    String choix = scanner.nextLine();
+    System.out.print(pseudo1 + " voulez-vous jouer les bleus ? ");
+    String bleu = scanner.nextLine();
 
-    while(!choix.equals("bleus") && !choix.equals("jaunes")){
-      System.out.print(pseudo1 + " veux-tu jouer les bleus ou les jaunes ? ");
-      choix = scanner.nextLine();
+    while (!bleu.equals("oui") && !bleu.equals("non")) {
+      System.out.print("Saisie incorrecte !");
+      System.out.print(pseudo1 + " voulez-vous jouer avec les bleus ? ");
+      bleu = scanner.nextLine();
     }
 
-    if (choix.equals("bleus")) {
-      pseudoBlanc=pseudo1;
-      pseudoNoir=pseudo2;
-    }
-    else if (choix.equals("jaunes")) {
-      pseudoBlanc=pseudo2;
-      pseudoNoir=pseudo1;
+    if (bleu.equals("oui")) {
+      pseudoBlanc = pseudo1;
+      pseudoNoir = pseudo2;
+    } else if (bleu.equals("non")) {
+      pseudoBlanc = pseudo2;
+      pseudoNoir = pseudo1;
     }
 
     methodes.remplir(plateau);
 
-    for(int tour = 0; tour < 100; tour++){
+    for (int tour = 0; tour < 100; tour++) {
 
-      if(tour % 2 == 0) {
+      if (tour % 2 == 0) {
         System.out.println("Au tour de " + pseudoBlanc + " (bleus)");
         if (methodes.estEnEchec(plateau, 'B')) {
           System.out.println(pseudoBlanc + " : votre Roi est en échec, vous devez le parer");
