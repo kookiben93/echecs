81ad06f5a9f0d032e112d61357b38d685b6bd1a2
Achille444 <achille.valesc@gmail.com>
2025-12-28 22:57:39 +0100

    Update Main.java

--- 28decembre/Main.java
+++ 28decembre/Main.java
@@ -38,7 +38,7 @@ public class Main {
         System.out.print("Joueur 2 entrez votre pseudo : ");
         String pseudo2 = scanner.nextLine();
 
-        System.out.print(pseudo1 + " voulez-vous jouer les bleus ? ");
+        System.out.print(pseudo1 + " voulez-vous jouer avec les bleus ? ");
         String bleu = scanner.nextLine();
 
         while (!bleu.equals("oui") && !bleu.equals("non")) {
@@ -102,7 +102,7 @@ public class Main {
         else
             couleur = "jaune";
 
-        System.out.print("Voulez-vous abandonner ? ");
+        System.out.print("Voulez-vous abandonner la partie ? ");
         abandon = sc.nextLine();
 
         while (!abandon.equals("oui") && !abandon.equals("non")) {
