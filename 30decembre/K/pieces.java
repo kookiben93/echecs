d201b40f5c1718aad090c0a2aea9f3a04fbbf4a2
kookiben93 <kbenatto@condorcet93.fr>
2025-12-30 19:08:05 +0100

    Create pieces.java

---
+++ 30decembre/K/pieces.java
@@ -0,0 +1,124 @@
+import java.util.Scanner;
+
+public class pieces {
+
+    public static void pion(int[][] plateau, int ligne, int colonne, char joueur, int mode) {
+        int pion = plateau[ligne][colonne];
+
+        if (mode == 1) {
+            methodes.BougePion(plateau, ligne, colonne, joueur, pion);
+        }
+        else{
+            methodes.mouvementPion(plateau, ligne, colonne, pion);
+        }
+    }
+
+    //Méthode pour la Tour
+    public static void tour(int[][] plateau, int ligne, int colonne, int mode, char joueur) {
+        int couleur = plateau[ligne][colonne]; //couleur de la pièce
+
+        if(mode==1){
+            methodes.BougeTour(plateau, ligne, colonne, couleur, joueur);
+        } else{
+            methodes.destinationPiece(plateau, ligne, colonne, couleur);
+        }
+    }
+
+    //Méthode pour le Fou
+    public static void fou(int[][] plateau, int ligne, int colonne, int mode, char joueur) {
+        int couleur = plateau[ligne][colonne]; //couleur de la pièce
+
+        if(mode==1) {
+            methodes.BougeFou(plateau, ligne, colonne, couleur, joueur);
+        } else{
+            methodes.destinationPiece(plateau, ligne, colonne, couleur);
+        }
+    }
+
+    //Méthode pour le Roi
+    public static void roi(int[][] plateau, int ligne, int colonne, int mvtTourLoin, int mvtTourProche, int mvtRoi, char joueur, int mode) {
+        int couleur = plateau[ligne][colonne]; //couleur de la pièce
+
+        if (mode == 1) {
+            methodes.BougeRoi(plateau, ligne, colonne, joueur, couleur, mvtTourLoin, mvtTourProche, mvtRoi);
+        } else {
+            methodes.mouvementRoi(plateau, ligne, colonne, couleur);
+        }
+    }
+
+    //Méthode pour la Cavalier
+    public static void cavalier(int[][] plateau, int ligne, int colonne, int mode) {
+        int NvColonne;
+        int NvLigne;
+        String col = methodes.conversionEnString(colonne);
+        String NvCol = "";
+        int couleur = plateau[ligne][colonne]; //couleur de la pièce
+
+        if (mode == 1){
+            int hautBas = 0;    //valeur pour le mouvement Haut ou Bas selon la demande
+            int gaucheDroite = 0;   //valeur pour le mouvement gauche ou droite selon la demande
+
+            int direction = methodes.directionCavalier(plateau, ligne, colonne, couleur);
+
+            switch (direction) {
+                case 1:     //L Grand Haut Gauche
+                    hautBas = -2;
+                    gaucheDroite = -1;
+                    break;
+                case 2:        //L Grand Haut Droit
+                    hautBas = -2;
+                    gaucheDroite = 1;
+                    break;
+                case 3:         //L Petit Haut Gauche
+                    hautBas = -1;
+                    gaucheDroite = -2;
+                    break;
+                case 4:         //L Petit Haut Droit
+                    hautBas = -1;
+                    gaucheDroite = 2;
+                    break;
+                case 5:         //L Grand Bas Gauche
+                    hautBas = 2;
+                    gaucheDroite = -1;
+                    break;
+                case 6:         //L Grand Bas Droit
+                    hautBas = 2;
+                    gaucheDroite = 1;
+                    break;
+                case 7:         //L Petit Bas Gauche
+                    hautBas = 1;
+                    gaucheDroite = -2;
+                    break;
+                case 8:         //L Petit Bas Droit
+                    hautBas = 1;
+                    gaucheDroite = 2;
+                    break;
+            }
+            NvLigne = ligne + hautBas;
+            NvColonne = colonne + gaucheDroite;
+
+            System.out.println("Le cavalier s'est déplacé en L de la case (" + (ligne + 1) + ',' + col + ") jusqu'à la case (" + (NvLigne + 1) + "," + NvCol + ")");
+            plateau[ligne][colonne] = 0;
+            plateau[NvLigne][NvColonne] = couleur;
+        }
+        else{
+            methodes.destinationPiece(plateau, ligne, colonne, couleur);
+        }
+    }
+
+    public static void dame(int[][] plateau, int ligne, int colonne, char joueur, int mode) {
+        Scanner sc = new Scanner(System.in);
+        int choix;
+
+        do {
+            System.out.print("1 pour aller en ligne droite, 2 pour aller en diagonale : ");
+            choix = Integer.parseInt(sc.nextLine());
+        } while (choix != 1 && choix != 2);
+
+        if (choix == 1) {
+            tour(plateau, ligne, colonne, mode, joueur);
+        } else {
+            fou(plateau, ligne, colonne, mode, joueur);
+        }
+    }
+}
