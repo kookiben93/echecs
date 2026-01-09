75dac2a7fa0eb5aaedf92f5449e3a8a4f575f005
Achille444 <achille.valesc@gmail.com>
2026-01-09 15:23:29 +0100

    Update MethodesTest.java

--- pat qui marche/MethodesTest.java
+++ pat qui marche/MethodesTest.java
@@ -1,5 +1,4 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class MethodesTest {
@@ -8,7 +7,7 @@ class MethodesTest {
     void testConversionColEnInt() {
         assertEquals(0, Methodes.conversionColEnInt("a"), "cas colonne 0 minuscule");
         assertEquals(0, Methodes.conversionColEnInt("A"), "cas colonne 0 majuscule");
-        assertEquals(7, Methodes.conversionColEnInt("h"), "cas colonne 7 majuscule");
+        assertEquals(7, Methodes.conversionColEnInt("h"), "cas colonne 7 minuscule");
         assertEquals(10, Methodes.conversionColEnInt("x"), "cas rejouer");
         assertEquals(8, Methodes.conversionColEnInt("z"),"cas hors limite" );
     }
@@ -18,47 +17,128 @@ class MethodesTest {
         assertTrue(Methodes.mouvementCavalier(4, 3, 2, 2), "cas (4,3) vers (2,2)");
         assertTrue(Methodes.mouvementCavalier(4, 3, 2, 4), "cas (4,3) vers (2,4)");
         assertFalse(Methodes.mouvementCavalier(4, 3, 4, 5), " cas mouvement horizontal");
+        assertFalse(Methodes.mouvementCavalier(4, 3, 5, 3), " cas mouvement vertical");
+        assertFalse(Methodes.mouvementCavalier(4, 3, 4, 3), " cas aucun mouvement");
+    }
+
+    @Test
+    void testMouvementTour() {
+        int[][] plateau = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 6, 0, 0, 0},       //pion jaune (2,4)
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 9, 0, 0, 7, 0, 0, 0},       //fou bleu (6,1)        //tour bleue (6,4)
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        assertTrue(Methodes.mouvementTour(plateau, 6, 4, 6, 6), "cas mouvement horizontal");
+        assertTrue(Methodes.mouvementTour(plateau, 6, 4, 3, 4), "cas mouvement vertical");
+        assertTrue(Methodes.mouvementTour(plateau, 6, 4, 2, 4), "cas prise d'une pièce ennemie");
+        assertFalse(Methodes.mouvementTour(plateau, 6, 4, 6, 4), " cas aucun mouvement");
     }
 
     @Test
     void testEstEnEchec() {
-        int [][] plateau = new int[8][8];
-
-        plateau[7][4] = 5;  //Roi Jaune
-        plateau[0][4] = 7;  //Tour Bleue
+        int[][] plateau = {
+                {0, 0, 0, 0, 5, 0, 0, 0},       //roi jaune (0,4)
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 7, 0, 0, 0},       //tour bleue (6,4)
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
 
         assertTrue(Methodes.estEnEchec(plateau, 'J'), "cas échec");
 
-        plateau[5][4] = 6;  //Pion Jaune entre les deux
+        plateau[5][4] = 6;  //pion jaune entre les deux
         assertFalse(Methodes.estEnEchec(plateau, 'J'), "cas pas d'échec");
     }
 
     @Test
     void testMemeCouleur() {
-        int [][] plateau = new int[8][8];
+        int[][] plateau = {
+                {1, 0, 0, 0, 0, 0, 0, 0},       //tour jaune (0,0)
+                {6, 0, 0, 0, 0, 0, 0, 0},       //pion jaune (1,0)
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {7, 0, 0, 0, 0, 0, 0, 0}        //tour bleu (0,7)
+        };
 
-        plateau[0][0] = 1; // Tour Jaune
-        plateau[0][1] = 6; // Pion Jaune
-        plateau[7][7] = 7; // Tour Bleue
-
-        assertTrue(Methodes.memeCouleur(plateau, 0, 1, 1), "cas de même couleur");
-        assertFalse(Methodes.memeCouleur(plateau, 7, 7, 1), "cas de couleur différente");
+        assertTrue(Methodes.memeCouleur(plateau, 0, 0, 6), "cas de même couleur");
+        assertFalse(Methodes.memeCouleur(plateau, 0, 7, 6), "cas de couleur différente");
         assertFalse(Methodes.memeCouleur(plateau, 5, 5, 1), "cas case vide");
     }
 
     @Test
     void testMaterielInsuffisant() {
-        int [][] plateau = new int[8][8];
+        int[][] plateau = {
+                {0, 0, 0, 0, 5, 0, 0, 0},       //roi jaune (0,4)
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 10, 0, 0, 0}       //roi bleu (7, 4)
+        };
 
-        plateau[0][4] = 5;  //Roi Jaune
-        plateau[7][4] = 10; //Roi Bleue
         assertTrue(Methodes.MaterielInsuffisant(plateau), "cas uniquement deux rois");
 
-
-        plateau[0][2] = 3;  //Fou Jaune
+        plateau[0][2] = 3;  //fou jaune
         assertTrue(Methodes.MaterielInsuffisant(plateau), "cas roi+fou contre roi");
 
-        plateau[3][3] = 4;  //Dame Jaune
+        plateau[3][3] = 4;  //dame jaune
         assertFalse(Methodes.MaterielInsuffisant(plateau), "cas materiel suffisant");
     }
+
+    @Test
+    void testPat() {
+
+        int[][] plateau1 = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 5, 0, 0, 0},       // roi jaune (4,4)
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 4, 0, 0},       // dame jaune (6,5)
+                {0, 0, 0, 0, 0, 0, 0, 10}       // roi bleu (7,7)
+        };
+        assertTrue(Methodes.pat(plateau1, 'B'), "cas pat");
+
+        int[][] plateau2 = {
+                {0, 0, 0, 0, 4, 0, 0, 0},       // dame jaune (0,4)
+                {5, 0, 0, 0, 0, 0, 0, 0},       //roi jaune (1, 0)
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 10, 0, 0, 0}       // roi bleu (7, 4)
+        };
+        assertFalse(Methodes.pat(plateau2, 'B'), "cas pas pat (le roi bleu peut bouger)");
+
+        int[][] plateau3 = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 10, 0, 0, 0},      //roi bleu (4,4)
+                {6, 0, 0, 0, 0, 0, 0, 0},       //pion jaune (5,0)
+                {0, 0, 0, 0, 0, 11, 0, 0},      //dame bleu (6,5)
+                {0, 0, 0, 0, 0, 0, 0, 5}       // roi jaune (7, 7)
+        };
+        assertFalse(Methodes.pat(plateau3, 'J'), "cas pas pat (le pion jaune peut bouger)");
+
+
+    }
+
 }
