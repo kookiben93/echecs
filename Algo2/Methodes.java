8f83b2c1747a620b7722b7d805b22f7aed4c57f5
Achille444 <achille.valesc@gmail.com>
2026-01-15 00:34:31 +0100

    Implement various array manipulation methods

---
+++ Algo2/Methodes.java
@@ -0,0 +1,110 @@
+import java.util.Random;
+
+public class Methodes {
+
+    public static void triInsertion(int[] tab) {
+
+        int temp;
+        int j;
+        for (int i = 1; i < tab.length - 1; i++) {
+            j = i - 1;
+            while ((j >= 0) && (tab[j] > tab[j + 1])) {
+                temp = tab[j + 1];
+                tab[j + 1] = tab[j];
+                tab[j] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static int[] trierCroissant(int longueur) {
+        int[] t = new int[longueur];
+        for (int i = 0; i < t.length; i++)
+            t[i] = i;
+        return t;
+    }
+
+    public static int[] trierDecroissant(int longueur) {
+        int[] t = new int[longueur];
+        for (int i = 0; i < t.length; i++)
+            t[i] = t.length - i;
+        return t;
+    }
+
+    /*
+        Retourne un tableau de int de la longueur demandée rempli avec des valeurs aléatoires
+        (valeurs comprises entre Integer.MIN_VALUE=-2^31 et Integer.MAX_VALUE=2^31-1 inclus).
+    */
+    public static int[] générerDonnées(int longueur) {
+        Random random = new Random();
+        int[] t = new int[longueur];
+        for (int i = 0; i < t.length; i++)
+            t[i] = random.nextInt(); // tirage aléatoire d'un int quelconque (compris entre -2^31 et 2^31-1)
+        return t;
+    }
+
+    /*
+        Retourne un tableau de int de la longueur demandée rempli avec des valeurs aléatoires comprises entre min et max inclus.
+    */
+    public static int[] générerDonnéesEntreBornes(int longueur, int min, int max) {
+        Random random = new Random();
+        int[] t = new int[longueur];
+        for (int i = 0; i < t.length; i++)
+            t[i] = random.nextInt(max - min + 1) + min; // nextInt(borne) retourne un int compris entre 0 inclus et borne exclus.
+        return t;
+    }
+
+    public static void afficher(int[] tab) {
+        for (int i = 1; i < tab.length; i++) {
+            System.out.print(tab[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //La question 3.a
+    public static int valeurLaPlusProche(int[] tab, int valeur) {
+        int plusProche = tab[0];
+        int i = 0;
+
+        while (i < tab.length && plusProche != valeur) {
+            int distanceActuelle = Math.abs(tab[i] - valeur);
+            int distancePlusProche = Math.abs(plusProche - valeur);
+
+            if (distanceActuelle < distancePlusProche)
+                plusProche = tab[i];
+            else if (distanceActuelle == distancePlusProche && plusProche < tab[i])
+                plusProche = tab[i];
+            i++;
+        }
+        return plusProche;
+    }
+
+    //La questions 3.b
+    public static int valeurLaPlusProcheDichotomique(int[] tab, int valeur) {
+        int indDebut = 0;
+        int indFin = tab.length - 1;
+        int indMilieu;
+        int plusProche = tab[0];
+
+        while (indDebut <= indFin) {
+            indMilieu = (indDebut + indFin) / 2;
+            int distanceActuelle = Math.abs(tab[indMilieu] - valeur);
+            int distancePlusProche = Math.abs(plusProche - valeur);
+
+            if (distanceActuelle < distancePlusProche) {
+                plusProche = tab[indMilieu];
+            } else if (distanceActuelle == distancePlusProche && tab[indMilieu] > plusProche) {
+                plusProche = tab[indMilieu];
+            }
+
+            if (tab[indMilieu] == valeur) {
+                return tab[indMilieu];
+            } else if (valeur > tab[indMilieu]) {
+                indDebut = indMilieu + 1;
+            } else {
+                indFin = indMilieu - 1;
+            }
+        }
+        return plusProche;
+    }
+}
