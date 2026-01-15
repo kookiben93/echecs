0adc965625fbee65e4fff66b93bb273c05284f1f
kookiben93 <kbenatto@condorcet93.fr>
2026-01-15 03:40:06 +0100

    Refactor triParInsertion.c for clarity and comments

--- Algo2/triParInsertion.c
+++ Algo2/triParInsertion.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#define TAILLE 20000
+#include <stdio.h>        //pour print f
+#include <stdlib.h>        //pour rand et srand
+#include <time.h>           //pour le temps (utilisé dans srand)
+#define TAILLE 20000        //taille définie à 20 000 (taille utilisé par le tableau)
 
 void triInsertion(int tab[], int longueur);
 
 int main() {
-    int tab[TAILLE];
-    int i;
-    int max=50;
+    int tab[TAILLE];        //initialisation d'un tableau de taille TAILLE
+    int max=50;            //valeur du nombre le plus grand pouvant être généré dans le teableau aléatoirement
 
-    srand(time(NULL));
+    srand(time(NULL));        //initialisation du générateur aléatoire avec l'heure actuelle
 
-    for (i = 0; i < TAILLE; i++) {
-        tab[i] = rand() % (max+1);
+    for (i = 0; i < TAILLE; i++) {        //boucle de remplissage du tableau avec des valeurs aléatoires
+        tab[i] = rand() % (max+1);        //comprises entre 0 et max (50 ici)
     }
 
-    triInsertion(tab, TAILLE);
+    triInsertion(tab, TAILLE);            //appel de la méthode de tri du tableau (tri par insertion)
 
-    for(int i = 0; i < TAILLE; i++){
+    for(int i = 0; i < TAILLE; i++){      //boucle d'affichage du tableau après le tri
         printf("%d ", tab[i]);
     }
 }
