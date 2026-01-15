bd706b1f210fb3a35c76939a8513031de2c47901
Achille444 <achille.valesc@gmail.com>
2026-01-15 05:32:50 +0100

    Update MethodesTest.java

--- Algo2/MethodesTest.java
+++ Algo2/MethodesTest.java
@@ -14,8 +14,8 @@ class MethodesTest {
         assertEquals(1, Methodes.valeurLaPlusProche(tab, -999), "cas négatif plus petit que toutes les valeurs du tableau");
         assertEquals(18, Methodes.valeurLaPlusProche(tab, 999),"cas positif plus grand que toutes les valeurs du tableau" );
 
-        int[] tab = {};
-        assertEquals(-1, Methodes.valeurLaPlusProche(tab, 8), "cas tableau vide");
+        int[] tab2 = {};
+        assertEquals(-1, Methodes.valeurLaPlusProche(tab2, 8), "cas tableau vide");
     }
     
     @Test
@@ -24,13 +24,13 @@ class MethodesTest {
         
         assertEquals(8, Methodes.valeurLaPlusProche(tab, 8), "cas valeur présente");
         assertEquals(17, Methodes.valeurLaPlusProche(tab , 16), "cas 2 valeurs à même distance");
-        assertEquals(9, Methodes.valeurLaPlusProche(tab, 10), "cas moyen");
+        assertEquals(8, Methodes.valeurLaPlusProche(tab, 10), "cas moyen");
         assertEquals(1, Methodes.valeurLaPlusProche(tab, -999), "cas négatif plus petit que toutes les valeurs du tableau");
         assertEquals(20, Methodes.valeurLaPlusProche(tab, 999), "cas positif plus grand que toutes les valeurs du tableau" );
         assertEquals(20, Methodes.valeurLaPlusProche(tab, 20), "cas limite valeur présente, la plus à droite");
         assertEquals(1, Methodes.valeurLaPlusProche(tab, 1), "cas limite valeur présente, la plus à gauche" );
 
-        int[] tab = {};
-        assertEquals(-1, Methodes.valeurLaPlusProche(tab, 8), "cas tableau vide");
+        int[] tab2 = {};
+        assertEquals(-1, Methodes.valeurLaPlusProche(tab2, 8), "cas tableau vide");
     }
 }
