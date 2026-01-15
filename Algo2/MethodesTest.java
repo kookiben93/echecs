174069683101ceb0233cc48cf0251b21afb2958f
kookiben93 <kbenatto@condorcet93.fr>
2026-01-15 04:57:40 +0100

    Update MethodesTest.java

--- Algo2/MethodesTest.java
+++ Algo2/MethodesTest.java
@@ -4,14 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MethodesTest {
 
-    int[] tab = {12, 1, 8, 6, 18, 9};
-
     @Test
     void TestvaleurLaPlusProche() {
+        int[] tab = {12, 1, 8, 6, 18, 9};
+        
         assertEquals(8, Methodes.valeurLaPlusProche(tab, 8), "cas valeur présente");
         assertEquals(8, Methodes.valeurLaPlusProche(tab ,7), "cas 2 valeurs à même distance");
         assertEquals(12, Methodes.valeurLaPlusProche(tab, 14), "cas moyen");
         assertEquals(1, Methodes.valeurLaPlusProche(tab, -999), "cas négatif plus petit que toutes les valeurs du tableau");
         assertEquals(18, Methodes.valeurLaPlusProche(tab, 999),"cas positif plus grand que toutes les valeurs du tableau" );
+    }
+    
+    @Test
+    void valeurLaPlusProcheDichotomique() {
+        int[] tab = {1, 2, 8, 15, 15, 17, 18, 19, 20};
+        
+        assertEquals(8, Methodes.valeurLaPlusProche(tab, 8), "cas valeur présente");
+        assertEquals(17, Methodes.valeurLaPlusProche(tab , 16), "cas 2 valeurs à même distance");
+        assertEquals(9, Methodes.valeurLaPlusProche(tab, 10), "cas moyen");
+        assertEquals(1, Methodes.valeurLaPlusProche(tab, -999), "cas négatif plus petit que toutes les valeurs du tableau");
+        assertEquals(20, Methodes.valeurLaPlusProche(tab, 999), "cas positif plus grand que toutes les valeurs du tableau" );
+        assertEquals(20, Methodes.valeurLaPlusProche(tab, 20), "cas limite valeur présente, la plus à droite");
+        assertEquals(1, Methodes.valeurLaPlusProche(tab, 1), "cas limite valeur présente, la plus à gauche" );
     }
 }
